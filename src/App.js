1b8d4b159ca493682a735efbc369d6dc28bbad6f
amurji22 <amurji22@uwo.ca>
2023-11-21 11:54:01 -0500

    log in button, email validation, error messages
    User now has a log in button with all the error handling in place for.
    - Users email is verified
    - Users email not verified
    - User account not created
    Additionally system checks for an @ and . in the email field before enabling either button

--- src/App.js
+++ src/App.js
@@ -1,53 +1,136 @@
-import React, { useState, useEffect } from 'react'; 
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { createUserWithEmailAndPassword, sendEmailVerification } from 'firebase/auth';
-import { auth } from "./config/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { auth } from './config/firebase.config';
 
 function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [user, setUser] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+
+  const [isValidEmail, setIsValidEmail] = useState(true); // New state for email validity
 
   const signUpAction = async () => {
-    await createUserWithEmailAndPassword(auth, email, password).then(async (userCred) => {
+    try {
+      const userCred = await createUserWithEmailAndPassword(auth, email, password);
       const user = userCred.user;
       await sendEmailVerification(user);
-      console.log("Success");
-    });
+      setSuccessMessage('Sign up successful! Please check your email for verification.');
+      setErrorMessage(null);
+      setEmail(''); // Clear email field
+      setPassword(''); // Clear password field
+      setIsValidEmail(true); // Reset email validity
+    } catch (error) {
+      console.error(error.message);
+      setErrorMessage(error.message);
+    }
   };
 
-  useEffect(() => { 
+  const loginAction = async () => {  
+    try {
+      const userCred = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCred.user;
+      if (!user.emailVerified) {
+        setErrorMessage('Please verify your email before continuing to log in.');
+      } else {
+        setErrorMessage(null);
+        setSuccessMessage('Login successful!');
+      }
+    } catch (error) {
+      console.error(error.message);
+  
+      switch (error.code) {
+        case 'auth/invalid-login-credentials':
+          setErrorMessage('Invalid email or password entered or no account found with that email');
+          break;
+        case 'auth/user-disabled':
+          setErrorMessage('Please contact the site administrator your account is disabled');
+          break;
+        default:
+          setErrorMessage('An error occurred. Please try again.');
+      }
+    }
+  };
+
+  useEffect(() => {
     auth.onAuthStateChanged((userCred) => {
-      const { email, emailVerified } = userCred;
-      setUser({ email, emailVerified });
+      if (userCred) {
+        const { email, emailVerified } = userCred;
+        setUser({ email, emailVerified });
+      } else {
+        setUser(null);
+      }
     });
   }, []);
 
   return (
     <main>
       <div className="container">
-        <InputField type={"email"} placeholder={"Enter your Email"} handleChange={(data) => setEmail(data)} />
-        <InputField type={"password"} placeholder={"Enter your Password"} handleChange={(data) => setPassword(data)} />
-        <button type="button" onClick={signUpAction}>Sign Up</button>
+        <InputField
+          type={'email'}
+          placeholder={'Enter your Email'}
+          handleChange={(data) => setEmail(data)}
+          setIsValidEmail={setIsValidEmail}
+        />
+        <input
+          value={password}
+          type={'password'}
+          placeholder={'Enter your Password'}
+          onChange={(data) => setPassword(data.target.value)}
+        />
+        <button type="button" onClick={signUpAction} disabled={!isValidEmail}>
+          Sign Up
+        </button>
+        <button type="button" onClick={loginAction} disabled={!isValidEmail}>
+          Log In
+        </button>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+        {successMessage && <p className="success-message">{successMessage}</p>}
       </div>
       {user && (
         <div>
           <p>{user?.email}</p>
-          <p>{user?.emailVerified ? "True" : "False"}</p>
+          <p>{user?.emailVerified ? 'True' : 'False'}</p>
         </div>
       )}
     </main>
-  );
+  );  
 }
 
-function InputField({ placeholder, handleChange, type }) {
-  const [inputValue, setInputValue] = useState("");
+function InputField({ placeholder, handleChange, setIsValidEmail }) {
+  const [inputValue, setInputValue] = useState('');
+  const [isValidEmail, setIsValid] = useState(true);
+
   const handleChangeEvent = (e) => {
-    setInputValue(e.target.value);
-    handleChange(e.target.value);
+    const value = e.target.value;
+    setInputValue(value);
+
+    // Basic email validation by checking for the presence of '@' and '.'
+    const isValid = value.includes('@') && value.includes('.');
+    setIsValid(isValid);
+    setIsValidEmail(isValid); // Call the setIsValidEmail function passed from the parent component
+
+    handleChange(value);
   };
+
   return (
-    <input value={inputValue} type={type} placeholder={placeholder} onChange={handleChangeEvent} />
+    <div>
+      <input
+        value={inputValue}
+        type="email" // Make sure the input type is set to 'email'
+        placeholder={placeholder}
+        onChange={handleChangeEvent}
+        className={isValidEmail ? '' : 'invalid-input'}
+      />
+
+      {!isValidEmail && <p className="error-message">Invalid email format.</p>}
+    </div>
   );
 }
 
