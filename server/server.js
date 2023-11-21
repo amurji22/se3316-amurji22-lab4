ba0ba83845d32e150c98d392ddda340bb4019ca0
amurji22 <amurji22@uwo.ca>
2023-11-21 16:21:24 -0500

    Created server.js
    Got the backend working with node and express js. Have also added the put method to store email, password and nickname of each user. Now just need to call it from react

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const lowDb = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const app = express();
+
+const usersdb = lowDb(new FileSync('users.json', { defaultValue: [] }));
+
+app.use(express.json());
+
+// Adding a user to the DB
+app.put('/api/users', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const nickname = req.body.nickname;
+
+    // Check if username exists 
+    const existingUser = usersdb.find({ "username": username }).value();
+
+    if (existingUser) {
+        return res.status(400).send('User already signed up!');
+    }
+
+    // Create a new user 
+    const newUser = {
+        username: username,
+        password: password,
+        nickname: nickname
+    }
+
+    usersdb.push(newUser).write();
+    res.status(200).send('User added successfully: ' + newUser.username);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
