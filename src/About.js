ee91574b7bb9adcf860e050fd24e48e7d0a1d51d
amurji22 <amurji22@uwo.ca>
2023-11-21 22:51:42 -0500

    Added about section to webpage

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+    return (
+        <div className="ph3 pv2 fw-bold lh-copy white" style={{ fontFamily: 'comic sans ms'}}>
+            <p>About the website:</p>
+            <p>The website enables users to acess an advanced web application with features like 
+                authentication, hero lists, and reviews. It supports user account creation, editing of hero lists, 
+                and public accessibility for lists and comments.</p>
+        </div>
+    );
+}
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Login from './login';
+import About from './About';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Login />
+    <About />
   </React.StrictMode>
 );
 
